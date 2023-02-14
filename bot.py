44531f6bb9892ac6d62a38e202517c94b5d642e7
Venuja Thilakarathna <109645464+VenujASB@users.noreply.github.com>
2023-02-14 14:09:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
+
 import openai
 import pyrogram
+import os
 
 # Set up OpenAI API credentials
-openai.api_key = "YOUR_API_KEY_HERE"
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Set up Pyrogram client
 app = pyrogram.Client(
     "my_bot",
-    api_id=12345,
-    api_hash="YOUR_API_HASH_HERE",
-    bot_token="YOUR_BOT_TOKEN_HERE"
+    api_id=os.environ.get('TELEGRAM_API_ID'),
+    api_hash=os.environ.get('TELEGRAM_API_HASH'),
+    bot_token=os.environ.get('TELEGRAM_BOT_TOKEN')
 )
 
 # Define a function to send messages
@@ -19,27 +21,46 @@ def send_message(chat_id, text):
 # Define a function to handle the /start command
 @app.on_message(pyrogram.filters.command("start"))
 def start_command(client, message):
-    # Send a welcome message
-    send_message(message.chat.id, "Hi, I'm Chat Bot!")
+    # Send a welcome message with buttons
+    buttons = [
+        [
+            pyrogram.InlineKeyboardButton(
+                "Developer", url="https://example.com/developer"
+            ),
+            pyrogram.InlineKeyboardButton(
+                "GitHub", url="https://github.com/example"
+            )
+        ]
+    ]
+    message_text = "Hi, I'm Chat Bot!\n\nHow can I help you today?"
+    client.send_message(
+        chat_id=message.chat.id,
+        text=message_text,
+        reply_markup=pyrogram.InlineKeyboardMarkup(buttons)
+    )
 
 # Define a function to process incoming messages
-@app.on_message()
+@app.on_message(pyrogram.filters.text)
 def handle_message(client, message):
     # Get the message text
     message_text = message.text.lower()
 
-    # Use OpenAI's GPT-3 API to generate a response
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=message_text,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
+    # Check if the message is a greeting
+    if "hello" in message_text or "hi" in message_text:
+        send_message(message.chat.id, "Hello! How can I help you today?")
+    else:
+        # Use OpenAI's GPT-3 API to generate a response
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=message_text,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5
+        )
 
-    # Send the generated response back to the user
-    send_message(message.chat.id, response.choices[0].text)
+        # Send the generated response back to the user
+        send_message(message.chat.id, response.choices[0].text)
 
 # Run the Pyrogram client
 app.run()
