03515a3462b6b0d7a54e2f1b3c12b05705cc885f
Venuja Thilakarathna <109645464+VenujASB@users.noreply.github.com>
2023-02-14 13:51:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,52 @@
-import openai
+import os
+import logging
 import pyrogram
+import openai
 
-# Set up OpenAI API credentials
-openai.api_key = "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"
-
-# Set up Pyrogram client
-app = pyrogram.Client(
-    "ChatGPTBot",
-    api_id=18862638,
-    api_hash="2a4a8dc0c1f6c9cb65f9f144439558ae",
-    bot_token="6172113599:AAGZm96NR0vgzyWy9IFgrHiN6VSfyGXukaI"
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
-# Define a function to send messages
-def send_message(chat_id, text):
-    app.send_message(chat_id, text)
+# Load environment variables
+TELEGRAM_API_ID = os.environ['18862638']
+TELEGRAM_API_HASH = os.environ['2a4a8dc0c1f6c9cb65f9f144439558ae']
+TELEGRAM_BOT_TOKEN = os.environ['6172113599:AAGZm96NR0vgzyWy9IFgrHiN6VSfyGXukaI']
+OPENAI_API_KEY = os.environ['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']
 
-# Define a function to handle the /start command
-@app.on_message(pyrogram.filters.command("start"))
-def start_command(client, message):
-    # Send a welcome message
-    send_message(message.chat.id, "Hi, I'm Chat Bot!")
+# Set up Pyrogram and OpenAI API clients
+app = pyrogram.Client('chatbot', api_id=TELEGRAM_API_ID, api_hash=TELEGRAM_API_HASH, bot_token=TELEGRAM_BOT_TOKEN)
+openai.api_key = OPENAI_API_KEY
 
-# Define a function to process incoming messages
-@app.on_message()
-def handle_message(client, message):
-    # Get the message text
-    message_text = message.text.lower()
+# Define welcome message and buttons
+WELCOME_MESSAGE = 'Welcome to my chatbot!'
+DEVELOPER_BUTTON = pyrogram.InlineKeyboardButton('Developer', url='https://www.example.com/developer')
+GITHUB_BUTTON = pyrogram.InlineKeyboardButton('GitHub', url='https://github.com/example')
 
-    # Use OpenAI's GPT-3 API to generate a response
+# Define command handlers
+@app.on_message(pyrogram.filters.command(['start']))
+def start_command_handler(client, message):
+    # Send welcome message and buttons
+    buttons = [DEVELOPER_BUTTON, GITHUB_BUTTON]
+    client.send_message(chat_id=message.chat.id, text=WELCOME_MESSAGE, reply_markup=pyrogram.InlineKeyboardMarkup([buttons]))
+
+@app.on_message(pyrogram.filters.text)
+def text_message_handler(client, message):
+    # Generate response using OpenAI
+    prompt = message.text
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=message_text,
-        max_tokens=100,
+        engine='davinci',
+        prompt=prompt,
+        max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5
+        temperature=0.5,
     )
+    text = response.choices[0].text.strip()
+    
+    # Send response message
+    client.send_message(chat_id=message.chat.id, text=text)
 
-    # Send the generated response back to the user
-    send_message(message.chat.id, response.choices[0].text)
-
-# Run the Pyrogram client
+# Run the bot
 app.run()
