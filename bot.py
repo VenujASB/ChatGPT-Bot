8bfd8395d68811b21eda34dd72d6065b5a708a33
Venuja Thilakarathna <109645464+VenujASB@users.noreply.github.com>
2023-02-17 18:08:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def message_handler(_, message: Message):
 
     # Use the OpenAI GPT to generate a response
     response = openai.Completion.create(
-        engine="davinci", prompt=message.text, max_tokens=50
+        engine="davinci", prompt=message.text, max_tokens=100
     )
 
     # Reply with the generated response
